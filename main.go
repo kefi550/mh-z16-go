7c75f424810fa3c5ccbcf255c50b63a2097281c5
kefi550 <ytiryrstd550@gmail.com>
2023-08-21 00:01:53 +0900

    struct

--- main.go
+++ main.go
@@ -3,51 +3,50 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/tarm/serial"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+type mhz16 struct {
+	port serial.Port
+}
+
 var (
-	port = kingpin.Arg("port", "portName").Required().ExistingFile()
-	zero = kingpin.Flag("zero", "zero calibration mode").Bool()
+	getCommand         = []byte{0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}
+	calibrationCommand = []byte{0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78}
 )
 
-func main() {
-	kingpin.Parse()
+func (m *mhz16) initialize(portName string) error {
 	options := &serial.Config{
-		Name:        *port,
+		Name:        portName,
 		Baud:        9600,
 		ReadTimeout: 0,
 	}
 	port, err := serial.OpenPort(options)
 	if err != nil {
-		log.Fatalln("serial port cannot open: %w", err)
+		return fmt.Errorf("serial port cannot open: %w", err)
 	}
-	time.Sleep(time.Second * 1)
-	defer port.Close()
-
-	if *zero {
-		zeroCalibration(*port)
-		return
-	}
-	co2 := getCo2(*port)
-	fmt.Println(co2)
+	m.port = *port
+	return nil
 }
 
-func getCo2(port serial.Port) int {
-	command := [9]byte{0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}
-	n, err := port.Write(command[:])
+func (m *mhz16) close() error {
+	err := m.port.Close()
+	return err
+}
+
+func (m *mhz16) getCo2() (int, error) {
+	n, err := m.port.Write(getCommand)
 	if err != nil {
-		log.Fatalln("serial write error: %w", err)
+		return 0, fmt.Errorf("serial write error: %w", err)
 	}
 	res := make([]byte, 9)
 	readed := 0
 	for {
-		n, err = port.Read(res[readed:9])
+		n, err = m.port.Read(res[readed:9])
 		if err != nil {
-			log.Fatalln("serial read error: %w", err)
+			return 0, fmt.Errorf("serial read error: %w", err)
 		}
 		if n == 0 {
 			break
@@ -59,17 +58,40 @@ func getCo2(port serial.Port) int {
 	}
 	checksum := 0xff & (^(res[1] + res[2] + res[3] + res[4] + res[5] + res[6] + res[7]) + 1)
 	if res[8] != checksum {
-		log.Fatalln("checksum not match")
+		return 0, fmt.Errorf("checksum not match error")
 	}
-	result := int(res[2]) << 8 + int(res[3])
-	return result
+	result := int(res[2])<<8 + int(res[3])
+	return result, nil
 }
 
-func zeroCalibration(port serial.Port) error {
-	command := [9]byte{0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78}
-	n, err := port.Write(command[:])
+func (m *mhz16) zeroCalibration() error {
+	n, err := m.port.Write(calibrationCommand)
 	if err != nil || n != 9 {
-		log.Fatalln("serial write error: %w", err)
+		return fmt.Errorf("serial write error: %w", err)
 	}
-	return err
+	return nil
+}
+
+func main() {
+	portName := kingpin.Arg("port", "portName").Required().ExistingFile()
+	zero := kingpin.Flag("zero", "zero calibration mode").Bool()
+
+	kingpin.Parse()
+
+	m := mhz16{}
+	err := m.initialize(*portName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer m.close()
+
+	if *zero {
+		m.zeroCalibration()
+		return
+	}
+	co2, err := m.getCo2()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(co2)
 }
